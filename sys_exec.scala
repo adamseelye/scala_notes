dd259e701e9622d01edeec76a1ec6085b28ae504
adamseelye <88736207+adamseelye@users.noreply.github.com>
2022-07-03 14:17:16 -0600

    fixed typo

--- sys_exec.scala
+++ sys_exec.scala
@@ -3,7 +3,7 @@ import sys.process._
 
 object sys_exec extends App {
   println("This will send the output of 'ls -l' to the Scala terminal")
-  println("Use this framework to execute system commands with Scala")
+  println("Use this framework to execute system commands with Scala\n")
 
   val sysexec = "ls -l" !
 
