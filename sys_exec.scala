ec8d9d59b6cd3553a1e33cea472e48b1193a08a5
adamseelye <88736207+adamseelye@users.noreply.github.com>
2022-07-03 13:22:15 -0700

    added sys_exec.scala
    execute system commands with scala code

---
+++ sys_exec.scala
@@ -0,0 +1,19 @@
+import scala.language.postfixOps
+import sys.process._
+
+object sys_exec extends App {
+  println("This will send the output of 'ls -l' to the Scala terminal")
+  println("Use this framework to execute system commands with Scala")
+
+  val sysexec = "ls -l" !
+
+  println(sysexec)
+
+  println("")
+  println("To use commands requiring a pipe, use the '#|' operator:\n")
+
+  val pipe = "ls -la #| less" !
+
+  println(pipe)
+
+}
